0093bdfbfad3decb081cc7120992873e4acc42c2
Chris Wilson <chris@legendserver.info>
2024-03-21 22:03:01 -0400

    Fix broken links and add more motivational videos. Remove eyestrain mode and spinning mode. Adjust mercy option to send four items.

--- public/assets/widgets.js
+++ public/assets/widgets.js
@@ -1,12 +1,12 @@
 const motivationalVideos = [
   'https://www.youtube.com/embed/dQw4w9WgXcQ', // Never Gonna Give You Up - Rick Astley
   'https://www.youtube.com/embed/ZXsQAXx_ao0', // Just Do It - Shia LeBouf
-  'https://www.youtube.com/embed/KmtzQCSh6xk', // Numa Numa
-  'https://www.youtube.com/embed/QH2-TGUlwu4', // Nyan Cat
+  'https://www.youtube.com/embed/Cqd1Gvq-RBY', // Numa Numa
+  'https://www.youtube.com/embed/2yJgwwDcgV8', // Nyan Cat
   'https://www.youtube.com/embed/9bZkp7q19f0', // Gangnam Style
   'https://www.youtube.com/embed/MtN1YnoL46Q', // The Duck Song
   'https://www.youtube.com/embed/EwTZ2xpQwpA', // Chocolate Rain - Tay Zonday
-  'https://www.youtube.com/embed/rfh4Mhp-a6U', // Dramatic Chipmunk
+  'https://www.youtube.com/embed/y8Kyi0WNg40', // Dramatic Chipmunk
   'https://www.youtube.com/embed/2Z4m4lnjxkY', // Trololo
   'https://www.youtube.com/embed/ZZ5LpwO-An4', // He-Man What's Goin' On
   'https://www.youtube.com/embed/Z3ZAGBL6UBA', // Peanut Butter Jelly Time
@@ -14,12 +14,24 @@ const motivationalVideos = [
   'https://www.youtube.com/embed/A67ZkAd1wmI', // Caramelldansen
   'https://www.youtube.com/embed/EIyixC9NsLI', // Badger Badger Badger
   'https://www.youtube.com/embed/CsGYh8AacgY', // Charlie the Unicorn
+  'https://www.youtube.com/embed/ytWz0qVvBZ0', // Diggy Diggy Hole
+  'https://www.youtube.com/embed/1qN72LEQnaU', // Hamster Dance,
+  'https://www.youtube.com/embed/qItugh-fFgg', // All Your Base Are Belong to Us
+  'https://www.youtube.com/embed/dTAAsCNK7RA', // OK Go - Here it Goes Again
+  'https://www.youtube.com/embed/kfVsfOSbJY0', // Rebecca Black - Friday
+  'https://www.youtube.com/embed/1wnE4vF9CQ4', // Leek Spin
+  'https://www.youtube.com/embed/ykwqXuMPsoc', // Narwhals
+  'https://www.youtube.com/embed/ZSM5t_0LIi8', // I'm a firin' mah lazar
+  'https://www.youtube.com/embed/_7jpz_55EdM', // Sesame Street - Jack Black Finds an Octagon
+  'https://www.youtube.com/embed/mm-aovm1axQ', // Trogdor the Burninator (Song Only)
 ];
 
 const motivatePlayer = (playerName = 'a dead player') => {
   // Only one video at a time, please
   const existing = document.getElementById('motivational-video-container');
-  if (existing) { existing.parentElement.removeChild(existing); }
+  if (existing) {
+    existing.parentElement.removeChild(existing);
+  }
 
   const container = document.createElement('div');
   container.setAttribute('id', 'motivational-video-container');
@@ -39,10 +51,9 @@ const motivatePlayer = (playerName = 'a dead player') => {
   motivationalVideo.setAttribute('width', '560');
   motivationalVideo.setAttribute('height', '315');
   motivationalVideo.setAttribute('frameborder', '0');
-  motivationalVideo.setAttribute(
-    'src',
-    `${motivationalVideos[Math.floor(Math.random() * motivationalVideos.length)]}?controls=0&autoplay=1`,
-  );
+  const videoUrl = `${motivationalVideos[Math.floor(Math.random() * motivationalVideos.length)]}?controls=0&autoplay=1`;
+  console.info(videoUrl);
+  motivationalVideo.setAttribute('src', videoUrl);
   motivationalVideo.setAttribute('allow', 'autoplay; encrypted-media; repeat; picture-in-picture; web-share;')
   container.appendChild(motivationalVideo);
   document.body.appendChild(container);
@@ -61,7 +72,7 @@ const chooseFate = () => {
   fateExplanation.innerText = 'You have been killed by Death Link five times. Fate has granted you the power ' +
     'of retribution. What will you do with it?';
   fateContainer.appendChild(fateExplanation);
-  
+
   const saveYourself = document.createElement('div');
   saveYourself.classList.add('fate-option');
   saveYourself.innerText = 'Become Immortal';
@@ -81,12 +92,12 @@ const chooseFate = () => {
   beGenerous.innerText = 'Show Mercy';
   const bgDesc = document.createElement('div');
   bgDesc.classList.add('fate-description');
-  bgDesc.innerText = 'Send two items immediately';
+  bgDesc.innerText = 'Send four items immediately';
   beGenerous.appendChild(bgDesc);
   fateContainer.appendChild(beGenerous);
   beGenerous.addEventListener('click', () => {
-    // Send two items immediately
-    immediateItems = 2;
+    // Send four items immediately
+    immediateItems = 4;
     hideFate();
   });
 
@@ -104,6 +115,19 @@ const chooseFate = () => {
     hideFate();
   });
 
+  const dadJoke = document.createElement('div');
+  dadJoke.classList.add('fate-option');
+  dadJoke.innerText = 'Hear a Dad Joke';
+  const djDesc = document.createElement('div');
+  djDesc.classList.add('fate-description');
+  djDesc.innerText = 'Farrak tells you a punny joke.';
+  dadJoke.appendChild(djDesc);
+  fateContainer.appendChild(dadJoke);
+  dadJoke.addEventListener('click', () => {
+    // TODO: Call out to the joke API to get a joke
+    hideFate();
+  });
+
   const fateDecides = document.createElement('div');
   fateDecides.classList.add('fate-option');
   fateDecides.innerText = 'Let Farrak Decide';
@@ -115,10 +139,10 @@ const chooseFate = () => {
   fateDecides.addEventListener('click', () => {
     hideFate();
     const roll = Math.floor(Math.random() * 6);
-    switch(roll) {
+    switch (roll) {
       case 0:
         appendConsoleMessage('FATE: Turn that frown upside down!');
-        document.getElementById('main-content').classList.add('spinning');
+        // TODO: Find a replacement for spinning the UI
         break;
       case 1:
         appendConsoleMessage('FATE: Nothing happened.');
@@ -136,13 +160,14 @@ const chooseFate = () => {
         break;
       case 4:
         // Eyestrain mode
-        appendConsoleMessage('FATE: Eyestrain mode activated. Please refresh to restore original settings.');
-        document.body.style.backgroundColor = 'limegreen';
-        document.getElementById('main-content').style.backgroundColor = 'magenta';
+        appendConsoleMessage('FATE: You were gifted with an incredible joke.');
+        // TODO: Find a replacement for eyestrain mode
         break;
       case 5:
         // Refresh the window
-        if (slotName) { sendMessageToServer(`${slotName} is learning about cheese!`); }
+        if (slotName) {
+          sendMessageToServer(`${slotName} is learning about cheese!`);
+        }
         window.location = 'https://cheese.com/'
         break;
     }
@@ -153,5 +178,7 @@ const chooseFate = () => {
 
 const hideFate = () => {
   const existing = document.getElementById('fate-container');
-  if (existing) { existing.parentElement.removeChild(existing); }
+  if (existing) {
+    existing.parentElement.removeChild(existing);
+  }
 };
